bdaf0e5290cfc54e44e2f7273c6d617da76fb943
Nicolás Willemet <nwillemet18@gmail.com>
2026-09-18 16:47:33 -0300

    Configurar HashRouter y deploy automatico con GitHub Actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/EMOS-WEB">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );

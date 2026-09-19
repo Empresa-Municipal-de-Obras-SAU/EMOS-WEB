1d97aaa7e65f9576392a5ba1fd5e59b6785350fc
Nicolás Willemet <nwillemet18@gmail.com>
2026-09-19 12:19:30 -0300

    Node.js actualizado

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,9 @@ import { HashRouter } from "react-router-dom";
 import "./styles.css";
 import App from "./App.tsx";
 
-console.log("VERSION NUEVA EMOS - HASHROUTER", Date.now());
-console.log("URL:", window.location.href);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-      <div style={{ display: "none" }}>
-        HASHROUTER-TEST
-      </div>
       <App />
     </HashRouter>
   </StrictMode>,
